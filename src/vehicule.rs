3f65c4e55100d9631460634e8390393ad51037e5
eymeric <hatchchien@protonmail.com>
2026-04-09 12:18:33 +0200

    maj

--- src/vehicule.rs
+++ src/vehicule.rs
@@ -1,4 +1,4 @@
-pub struct vehicule {
+pub struct Vehicule {
     capacity: u8,
     charge: u8, // charge of the vehicule at the moment, charge never exceed capacity
     route: Vec<String>, // client ids
